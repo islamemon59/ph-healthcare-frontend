d26c98f688eb7a65148d28bf377f721bc4580809
Emon Hossain <islamemon59@gmail.com>
2026-03-02 05:55:47 +0600

    common protected route page added

---
+++ src/app/(dashboardLayout)/(commonProtectedLayout)/change-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChangePasswordPage = () => {
+    return (
+        <div>
+            This is change password page.
+        </div>
+    );
+};
+
+export default ChangePasswordPage;
