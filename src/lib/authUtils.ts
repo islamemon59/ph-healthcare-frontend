ffcfd0a2c314693517c7679ef2396764262c24b4
Emon Hossain <islamemon59@gmail.com>
2026-03-06 02:05:11 +0600

    authUtils function added

---
+++ src/lib/authUtils.ts
@@ -0,0 +1,83 @@
+export type UserRole = "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT";
+
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+  "/logout",
+];
+
+export const isAuthRoute = (pathName: string) => {
+  return authRoutes.some((route: string) => route === pathName);
+};
+
+export type RouteConfig = {
+  exact: string[];
+  pattern: RegExp[];
+};
+
+export const commonProtectedRoutes: RouteConfig = {
+  exact: ["/my-profile", "/change-password"],
+  pattern: [],
+};
+
+export const doctorProtectedRoutes: RouteConfig = {
+  pattern: [/^\/doctor\/dashboard/],
+  exact: [],
+};
+
+export const adminOrSuperProtectedRoutes: RouteConfig = {
+  pattern: [/^\/admin\/dashboard/],
+  exact: [],
+};
+
+export const patientProtectedRoutes: RouteConfig = {
+  pattern: [/^\/dashboard/],
+  exact: ["/payment/success"],
+};
+
+export const isRouteMatches = (pathName: string, route: RouteConfig) => {
+  if (route.exact.includes(pathName)) {
+    return true;
+  }
+  return route.pattern.some((pattern: RegExp) => pattern.test(pathName));
+};
+
+export const getRouteOwner = (
+  pathName: string,
+): "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT" | "COMMON" => {
+  if (isRouteMatches(pathName, commonProtectedRoutes)) {
+    return "COMMON";
+  }
+
+  if (isRouteMatches(pathName, doctorProtectedRoutes)) {
+    return "DOCTOR";
+  }
+
+  if (isRouteMatches(pathName, adminOrSuperProtectedRoutes)) {
+    return "ADMIN";
+  }
+
+  if (isRouteMatches(pathName, patientProtectedRoutes)) {
+    return "PATIENT";
+  }
+
+  return "COMMON";
+};
+
+export const getDefaultDashboardRoute = (role: UserRole) => {
+  if (role === "ADMIN" || role === "SUPER_ADMIN") {
+    return "/admin/dashboard";
+  }
+
+  if (role === "DOCTOR") {
+    return "/doctor/dashboard";
+  }
+
+  if (role === "PATIENT") {
+    return "/dashboard";
+  }
+
+  return "/";
+};
