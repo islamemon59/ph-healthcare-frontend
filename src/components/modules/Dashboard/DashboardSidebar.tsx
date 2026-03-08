369236e78f4fbc0df55a41b1bb78542d05823388
Emon Hossain <islamemon59@gmail.com>
2026-03-08 14:35:10 +0600

    navItems added in dahsboardSidebar

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { getDefaultDashboardRoute } from "@/src/lib/authUtils";
+import { getNavItemsByRole } from "@/src/lib/navItems";
+import { getUserInfo } from "@/src/services/auth.services";
+import { NavSection } from "@/src/types/dashboard.types";
 
-const DashboardSidebar = () => {
-    return (
-        <div>
-            Dashboard Sidebar
-        </div>
-    );
+const DashboardSidebar = async () => {
+  const userInfo = await getUserInfo();
+
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+
+  return <div>Dashboard Sidebar</div>;
 };
 
 export default DashboardSidebar;
